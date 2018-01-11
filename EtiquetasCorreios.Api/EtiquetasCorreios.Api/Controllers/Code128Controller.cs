e9d48f370e9ef5da5de93e76e41452dd7eceed66
Otávio <otaviolarrosa@outlook.com>
2018-01-10 22:39:01 -0200

    Feito API que retorna o Code-BAR 128

---
+++ EtiquetasCorreios.Api/EtiquetasCorreios.Api/Controllers/Code128Controller.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ZXing;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EtiquetasCorreios.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class Code128Controller : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public string Get(string valor)
+        {
+            BarcodeWriterPixelData writer = new BarcodeWriterPixelData() { Format = BarcodeFormat.CODE_128 };
+            writer.Options.PureBarcode = true;
+            var pixelData = writer.Write(valor);
+            using (var bitmap = new Bitmap(pixelData.Width, pixelData.Height, PixelFormat.Format32bppRgb))
+            {
+                var stream = new MemoryStream();
+                var bitmapData = bitmap.LockBits(new Rectangle(0, 0, pixelData.Width, pixelData.Height), ImageLockMode.WriteOnly, PixelFormat.Format32bppRgb);
+                try
+                {
+                    System.Runtime.InteropServices.Marshal.Copy(pixelData.Pixels, 0, bitmapData.Scan0, pixelData.Pixels.Length);
+                }
+                finally
+                {
+                    bitmap.UnlockBits(bitmapData);
+                }
+                bitmap.Save(stream, ImageFormat.Png);
+                return Convert.ToBase64String(stream.ToArray());
+            }
+        }
+    }
+}
